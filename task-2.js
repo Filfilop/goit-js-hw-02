8fe8036129a1448c610f4ab699802b116a19f243
Filfilop <64086672+Filfilop@users.noreply.github.com>
2020-08-22 20:36:40 +0300

    fdgfh

---
+++ task-2.js
@@ -0,0 +1,9 @@
+const invoice = 200;
+const stock = 100;
+
+const message =
+  invoice <= stock
+    ? "Заказ оформлен, с вами свяжется менеджер"
+    : "На складе недостаточно товаров!";
+
+console.log(message);
